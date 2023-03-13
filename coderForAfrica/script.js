a3445a60abd46c3a721e7948e27c3a6256d482b3
Jeremiah Egbune I <110425084+jegbune@users.noreply.github.com>
2023-03-13 23:26:43 +0100

    CoderForAfrica

---
+++ coderForAfrica/script.js
@@ -0,0 +1,14 @@
+let addCart = document.getElementsByClassName('addBtn');
+let addNumber = document.querySelector('.adNum');
+let cart = 0;
+
+
+addCart.addEventListener('click', function addtoCart() {
+    
+    console.log('add to cart');
+    
+    // cart =+ 1;
+    // addNumber.textcontent = cart;
+})
+
+// addtoCart();
